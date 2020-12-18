5364c23e5e8fe696a34c6ef25450c2f0b560cdb8
Сергей Шмигельский <siro181a@gmail.com>
2020-12-18 14:03:22 +0200

    fix

--- bundles/main.js
+++ bundles/main.js
@@ -1,23 +1,23 @@
-$(document).ready(function () {
+// $(document).ready(function () {
 
-    //E-mail Ajax Send
-    $("form").submit(function () { //Change
-        var th = $(this);
-        $.ajax({
-            type: "POST",
-            url: "mail.php", //Change
-            data: th.serialize()
-        }).done(function () {
-            alert("Thank you!");
-            setTimeout(function () {
-                // Done Functions
-                th.trigger("reset");
-            }, 1000);
-        });
-        return false;
-    });
+//     //E-mail Ajax Send
+//     $("form").submit(function () { //Change
+//         var th = $(this);
+//         $.ajax({
+//             type: "POST",
+//             url: "mail.php", //Change
+//             data: th.serialize()
+//         }).done(function () {
+//             alert("Thank you!");
+//             setTimeout(function () {
+//                 // Done Functions
+//                 th.trigger("reset");
+//             }, 1000);
+//         });
+//         return false;
+//     });
 
-});
+// });
 
 (function ($) {
     $(document).ready(function () {
@@ -101,50 +101,3 @@ $(function () {
     visitors();
     videoReview();
 });
-
-
-window.addEventListener("DOMContentLoaded", function () {
-
-    // get the form elements defined in your form HTML above
-
-    var form = document.getElementById("my-form");
-    var button = document.getElementById("my-form-button");
-    var status = document.getElementById("my-form-status");
-
-    // Success and Error functions for after the form is submitted
-
-    function success() {
-        form.reset();
-        button.style = "display: none ";
-        status.innerHTML = "Thanks!";
-    }
-
-    function error() {
-        status.innerHTML = "Oops! There was a problem.";
-    }
-
-    // handle the form submission event
-
-    form.addEventListener("submit", function (ev) {
-        ev.preventDefault();
-        var data = new FormData(form);
-        ajax(form.method, form.action, data, success, error);
-    });
-});
-
-// helper function for sending an AJAX request
-
-function ajax(method, url, data, success, error) {
-    var xhr = new XMLHttpRequest();
-    xhr.open(method, url);
-    xhr.setRequestHeader("Accept", "application/json");
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState !== XMLHttpRequest.DONE) return;
-        if (xhr.status === 200) {
-            success(xhr.response, xhr.responseType);
-        } else {
-            error(xhr.status, xhr.response, xhr.responseType);
-        }
-    };
-    xhr.send(data);
-}
